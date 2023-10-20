b20c0eb11021d5a1a6a0e40c9ca701db450364c8
AbuArwa001 <khalfanathman12@yahoo.com>
2023-10-20 12:29:42 +0300

    Add updated strlen file

--- 2-strlen.c
+++ 2-strlen.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _strlen - computes the size of a string
+ * _strlen - String length
  *
- * @s: the string to be examined
- * Return: returns the length in number
+ * @s: The string to be measured
+ *
+ * Return: The length of @s
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
-	int length = 0;
+	unsigned int l;
 
-	while (*s++)
-	{
-		length++;
-	}
-
-	return (length);
+	l = 0;
+	while (s && s[l])
+		++l;
+	return (l);
 }
